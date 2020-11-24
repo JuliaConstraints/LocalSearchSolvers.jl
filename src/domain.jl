ccc7bef1a14a27b62f1ca3050d176bf645a3e226
Azzaare <jf@baffier.fr>
2020-11-24 11:00:49 +0900

    Added domain structures and methods

--- src/domain.jl
+++ src/domain.jl
@@ -2,10 +2,17 @@
 abstract type AbstractDomain end
 
 ## Abstract Continuous Domain
+<<<<<<< HEAD
 abstract type ContinuousDomain{T <: AbstractFloat} <: AbstractDomain end
 
 # Continuous Interval structure
 struct ContinuousInterval{T <: AbstractFloat} <: ContinuousDomain{T}
+=======
+abstract type ContinuousDomain {T <: AbstractFloat} <: AbstractDomain end
+
+# Continuous Interval structure
+struct ContinuousInterval {T <: AbstractFloat} <: ContinuousInterval{T}
+>>>>>>> Added domain structures and methods
     start::T
     stop::T
     start_open::Bool
@@ -13,12 +20,20 @@ struct ContinuousInterval{T <: AbstractFloat} <: ContinuousDomain{T}
 end
 
 # Continuous Intervals
+<<<<<<< HEAD
 struct ContinuousIntervals{T <: AbstractFloat} <: ContinuousDomain{T}
+=======
+struct ContinuousInterval {T <: AbstractFloat} <: ContinuousInterval{T}
+>>>>>>> Added domain structures and methods
     intervals::Vector{ContinuousInterval{T}}
 end
 
 ## Abstract Discrete Domain
+<<<<<<< HEAD
 abstract type DiscreteDomain{T <: Number} <: AbstractDomain end
+=======
+abstract type DiscreteDomain {T <: Number} end
+>>>>>>> Added domain structures and methods
 
 # Set Domain
 struct SetDomain{T <: Number} <: DiscreteDomain{T}
@@ -28,11 +43,17 @@ end
 function SetDomain(values::Vector{T}) where T <: Number
     SetDomain(Set(values))
 end
+<<<<<<< HEAD
 
 # TODO: automatic conversion ?
 # function SetDomain(values::OrdinalRange{T}) where T <: Number
 #     SetDomain(Set(values))
 # end
+=======
+function SetDomain(values::OrdinalRange{T}) where T <: Number
+    SetDomain(Set(values))
+end
+>>>>>>> Added domain structures and methods
 
 # Indices Domain
 struct IndicesDomain{T <: Number} <: DiscreteDomain{T}
@@ -42,23 +63,40 @@ end
 
 function IndicesDomain(points::Vector{T}) where T <: Number
     inds = Dictionary{T,Int}(points, 1:length(points))
+<<<<<<< HEAD
     IndicesDomain(points, deepcopy(inds))
+=======
+    IndexesDomain(points, deepcopy(inds))
+>>>>>>> Added domain structures and methods
 end
 
 ### Methods
 _length(d::D) where D <: DiscreteDomain = length(d.points)
+<<<<<<< HEAD
 _get(d::IndicesDomain, index::Int) = d.points[index]
+=======
+_get(d::IndexesDomain, index::Int) = d.points[index]
+>>>>>>> Added domain structures and methods
 _draw(d::D) where D <: DiscreteDomain = rand(d.points)
 ∈(value::T, d::D) where {T <: Real,D <: DiscreteDomain} = value ∈ d.points
 _delete!(d::SetDomain{T}, value::T) where {T <: Real} = pop!(d.points, value)
 
 # TODO: implement delete! for ContinuousDomain
+<<<<<<< HEAD
 function _delete!(d::IndicesDomain{T}, value::T) where T <: Real
     index = get(d.inds, value, 0)
     if index > 0
         deleteat!(d.points, index)
         delete!(d.inds, value)
         map(((k,v),) -> v < index ? v : v - 1, pairs(d.inds))
+=======
+function _delete!(d::IndexesDomain{T}, value::T) where T <: Real
+    index = get(d.indexes, value, 0)
+    if index > 0
+        deleteat!(d.points, index)
+        delete!(d.indexes, value)
+        map(((k,v),) -> v < index ? v : v - 1, pairs(d.indexes))
+>>>>>>> Added domain structures and methods
     end
 end
 
@@ -68,6 +106,7 @@ function _add!(d::SetDomain{T}, value::T) where {T <: Real}
     end
 end
 
+<<<<<<< HEAD
 function _add!(d::IndicesDomain{T}, value::T) where {T <: Real}
     if !(value ∈ d)
         push!(d.points, value)
@@ -84,3 +123,11 @@ Discrete domain constructor.
 The `type` keyword can be set to `:set` (default) or `:indices`.
 """
 domain(values::Vector{T}; type = :set) where T <: Number = _domain(Val(type), values)
+=======
+function _add!(d::IndexesDomain{T}, value::T) where {T <: Real}
+    if !(value ∈ d)
+        push!(d.points, value)
+        insert!(d.indexes, value, length(d.points))
+    end
+end
+>>>>>>> Added domain structures and methods
