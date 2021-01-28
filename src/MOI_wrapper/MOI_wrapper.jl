0f37de137922f8da8b43c09053b882f59e4e1fda
Azzaare <jf@baffier.fr>
2021-01-28 14:41:12 +0900

    stage

--- src/MOI_wrapper/MOI_wrapper.jl
+++ src/MOI_wrapper/MOI_wrapper.jl
@@ -1,8 +1,8 @@
-mutable struct Optimizer <: MOI.AbstractOptimizer
-    inner::Union{Solver, Nothing}
-    variable_info::Vector{Variable}
-    # which variable index, (:leq,:geq,:eq,:Int,:Bin), and lower and upper bound
-    var_constraints::Vector{Tuple{Int64,Symbol,Int64,Int64}}
-    status::MOI.TerminationStatusCode
-    options::SolverOptions
-end
+# mutable struct Optimizer <: MOI.AbstractOptimizer
+#     inner::Union{Solver, Nothing}
+#     variable_info::Vector{Variable}
+#     # which variable index, (:leq,:geq,:eq,:Int,:Bin), and lower and upper bound
+#     var_constraints::Vector{Tuple{Int64,Symbol,Int64,Int64}}
+#     status::MOI.TerminationStatusCode
+#     options::SolverOptions
+# end
