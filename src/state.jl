7440b3702d295f5e40baa5978bd01de8a949c385
Jean-Francois Baffier <jf@baffier.fr>
2021-02-09 15:31:42 +0900

    Extend MathOptInterface to LocalSearchSolvers.jl (part 1. satisfaction) (#40)
    * Updated doc config for stable and dev doc release (#38)
    
    * temp save moi
    
    * Add files via upload (#39)
    
    * temp save moi
    
    * stage
    
    * stage 2/
    
    * temp save ...
    
    * created a solver options structure
    
    * temp save 2
    
    * Stabilize tests for CI
    
    * temp save + constraintDomains changes
    
    * Temp save again ...
    
    * Extended MOI interface to support discrete domains
    
    * Added MOI wrappers for constraints in usual constraints
    
    * Functional MOI wrapper for SAT problems
    
    * Fix for 32 bits architecture
    
    * Fix for 32 bits architecture 2/?
    
    * Added MOI objectives
    
    * Simplified Constraint constructor
    
    * Fix for constraint constructors 2/
    
    * Added basic JuMP requirements
    
    * JuMP model OK. optimize! bug 1/?
    
    * one needs to add the parametric type inside the Set (#55)
    
    * Fix (dirty) for 32 bits systems
    
    * Bug: @variable(m, X[1:N, 1:N], DiscreteSet(1:N))
    
    * Minimal JuMP interface working
    
    * Fixed sudoku problem in JuMP
    
    * JuMP syntax for global constraint is more concise
    
    * Generic function for sudoku instance with an optional modeler that default to JuMP
    
    * Updated settings to options. Bug in tabu times.
    
    * Corrected tabu bug
    
    * MOI options and status
    
    * Solutions and termination status
    
    * Bug with Ref and value
    
    * Corrected indices typo in sudoku(JuMP)
    
    * Improved display access in sudoku(JuMP)
    
    * MOI step 1 (CSP problems)
    
    Co-authored-by: Ole Kröger <o.kroeger@opensourc.es>

--- src/state.jl
+++ src/state.jl
@@ -107,13 +107,13 @@ _satisfying!(s::_State) = s.optimizing = false
     _cons_cost(s::S, c) where S <: Union{_State, AbstractSolver}
 Return the cost of constraint `c`.
 """
-_cons_cost(s::_State, c) = _cons_costs(s)[c]
+_cons_cost(s::_State, c) = get!(_cons_costs(s), c, 0.0)
 
 """
     _var_cost(s::S, x) where S <: Union{_State, AbstractSolver}
 Return the cost of variable `x`.
 """
-_var_cost(s::_State, x) = _vars_costs(s)[x]
+_var_cost(s::_State, x) = get!(_vars_costs(s), x, 0.0)
 
 """
     _value(s::S, x) where S <: Union{_State, AbstractSolver}
@@ -179,7 +179,8 @@ _solution!(s::_State, values) = s.best_solution = copy(values)
     _best!(s::S, val, values = Dictionary()) where S <: Union{_State, AbstractSolver}
 Set the best known value to `val` and, if `values` not empty, the best known solution.
 """
-function _best!(s::_State, val::T, values = Dictionary{Int,T}()) where {T <: Number}
+function _best!(s::_State, val::Union{Nothing,T}, values=Dictionary{Int,T}()
+) where {T <: Number}
     if isnothing(_best(s)) || val < _best(s)
         s.best_solution_value = val
         s.best_solution = copy(isempty(values) ? s.values : values)
@@ -238,4 +239,15 @@ Within the non-tabu variables, select the one with the worse error .
 function _select_worse(s::_State)
     nontabu = setdiff(keys(_vars_costs(s)), keys(_tabu(s)))
     return _find_rand_argmax(view(_vars_costs(s), nontabu))
+end
+
+function empty!(s::_State)
+    empty!(s.values)
+    empty!(s.vars_costs)
+    empty!(s.cons_costs)
+    _error!(s, 0.0)
+    empty!(s.tabu)
+    _satisfying!(s)
+    empty!(s.best_solution)
+    s.best_solution_value = nothing
 end
--- test/internal.jl
+++ test/internal.jl
@@ -4,47 +4,48 @@ d2 = domain([4,3,2,1]; type=:indices)
 domains = Dictionary(1:2, [d1, d2])
 
 # get
-@test LocalSearchSolvers._get(d2, 2) == 3
+@test LS._get(d2, 2) == 3
 for d in domains
     # constructors and ∈
     for x in [1,2,3,4]
         @test x ∈ d
     end
     # length
-    @test LocalSearchSolvers._length(d) == 4
+    @test LS._length(d) == 4
     # draw and ∈
-    @test LocalSearchSolvers._draw(d) ∈ d
+    @test LS._draw(d) ∈ d
     # add!
-    LocalSearchSolvers._add!(d, 5)
+    LS._add!(d, 5)
     @test 5 ∈ d
     # delete!
-    LocalSearchSolvers._delete!(d, 5)
+    LS._delete!(d, 5)
     @test 5 ∉ d
 end
 
 
 ## test variables
-x1 = variable([4,3,2,1], "x1")
-x2 = variable(d2, "x2")
+x1 = variable([4,3,2,1])
+x2 = variable(d2)
+x3 = variable() # TODO: tailored test for free variable
 vars = Dictionary(1:2, [x1, x2])
 
-@test LocalSearchSolvers._get(x2, 2) == 3
+@test LS._get(x2, 2) == 3
 for x in vars
     # add and delete from constraint
-    LocalSearchSolvers._add_to_constraint!(x, 1)
-    LocalSearchSolvers._add_to_constraint!(x, 2)
-    LocalSearchSolvers._delete_from_constraint!(x, 2)
+    LS._add_to_constraint!(x, 1)
+    LS._add_to_constraint!(x, 2)
+    LS._delete_from_constraint!(x, 2)
     @test x ∈ 1
     @test x ∉ 2
-    @test LocalSearchSolvers._constriction(x) == 1
-    @test LocalSearchSolvers._length(x) == 4
+    @test LS._constriction(x) == 1
+    @test LS._length(x) == 4
     for y in [1,2,3,4]
         @test y ∈ x
     end
-    @test LocalSearchSolvers._draw(x) ∈ x
-    LocalSearchSolvers._add!(x, 5)
+    @test LS._draw(x) ∈ x
+    LS._add!(x, 5)
     @test 5 ∈ x
-    LocalSearchSolvers._delete!(x, 5)
+    LS._delete!(x, 5)
     @test 5 ∉ x
 end
 
@@ -52,16 +53,16 @@ end
 values = [1, 2, 3]
 inds = [1, 2]
 err = error_f(usual_constraints[:all_different])
-c1 = constraint(err, inds, values)
-c2 = constraint(err, inds, vars)
+c1 = constraint(err, inds)
+c2 = constraint(err, inds)
 cons = Dictionary(1:2, [c1, c2])
 
 for c in cons
-    LocalSearchSolvers._add!(c, 3)
+    LS._add!(c, 3)
     @test 3 ∈ c
-    LocalSearchSolvers._delete!(c, 3)
+    LS._delete!(c, 3)
     @test 3 ∉ c
-    @test LocalSearchSolvers._length(c) == 2
+    @test LS._length(c) == 2
     c.f(values; dom_size=3)
 end
 
@@ -78,8 +79,8 @@ end
 m = Model()
 @test_logs describe(m)
 
-x1 = variable([4,3,2,1], "x1")
-x2 = variable(d2, "x2")
+x1 = variable([4,3,2,1])
+x2 = variable(d2)
 vars = Dictionary(1:2, [x1, x2])
 for x in vars
     add!(m, x)
@@ -88,8 +89,8 @@ variable!(m, d1)
 
 values = [1, 2, 3]
 inds = [1, 2]
-c1 = constraint(err, inds, values)
-c2 = constraint(err, inds, vars)
+c1 = constraint(err, inds)
+c2 = constraint(err, inds)
 cons = Dictionary(1:2, [c1, c2])
 for c in cons
     add!(m, c)
