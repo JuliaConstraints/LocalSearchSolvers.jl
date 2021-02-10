26646551b9e1e66baa58318aa63380afe78a6de9
Azzaare <jf@baffier.fr>
2021-02-10 16:22:42 +0900

    Tests cleaning

--- test/functions.jl
+++
@@ -1,3 +0,0 @@
-v = [1,2,3,4]
-
-@test o_dist_extrema(v) == 3.0
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,8 @@ import Constraints: usual_constraints, error_f
 const LS = LocalSearchSolvers
 
 @testset "LocalSearchSolvers.jl" begin
-    # include("internal.jl")
-    # include("problems.jl")
-    # include("functions.jl")
+    include("internal.jl")
+    include("raw_solver.jl")
     include("MOI_wrapper.jl")
+    include("JuMP.jl")
 end
