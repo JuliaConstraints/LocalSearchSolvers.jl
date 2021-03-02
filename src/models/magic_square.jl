39de359e251c0d779a6e032c9ea45ad1462a3d5e
azzaare <jf@baffier.fr>
2021-03-02 16:01:36 +0900

    Added documentation for magic_square

--- src/models/magic_square.jl
+++ src/models/magic_square.jl
@@ -1,10 +1,8 @@
-sum_equal_param(param) = x -> sum(x) == param
-
 function magic_square(n, ::Val{:JuMP})
     N = n^2
     model = JuMP.Model(CBLS.Optimizer)
     magic_constant = n * (N + 1) / 2
-    Σeq = sum_equal_param(magic_constant)
+    Σeq = x -> sum(x) == magic_constant
 
     @variable(model, X[1:n, 1:n], DiscreteSet(1:N))
     @constraint(model, vec(X) in AllDifferent())
@@ -19,6 +17,11 @@ function magic_square(n, ::Val{:JuMP})
     return model, X
 end
 
+"""
+    magic_square(n; modeler = :JuMP)
+
+Create a model for the magic square problem of order `n`. The `modeler` argument accepts :JuMP (default), which refer to the solver the JuMP model.
+"""
 magic_square(n; modeler = :JuMP) = magic_square(n, Val(modeler))
 
 
