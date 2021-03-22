4c487dccb362fb1e60fcc765b29e79497a2fbc2f
Azzaare <jf@baffier.fr>
2021-03-22 13:00:22 +0900

    Updated coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,13 @@
 import ConstraintDomains
+using ConstraintModels
+using Constraints
 using Dictionaries
 using LocalSearchSolvers
 using Test
-
-import Constraints: usual_constraints, error_f
 
 const LS = LocalSearchSolvers
 
 @testset "LocalSearchSolvers.jl" begin
     include("internal.jl")
-    # include("raw_solver.jl")
-    # include("MOI_wrapper.jl")
-    # include("JuMP.jl")
+    include("raw_solver.jl")
 end
