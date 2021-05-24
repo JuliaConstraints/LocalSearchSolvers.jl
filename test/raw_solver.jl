0115fbc9953294f45afc35573d61e1bcd334ffcf
azzaare <jf@baffier.fr>
2021-05-24 09:54:31 +0900

    Temp save

--- test/raw_solver.jl
+++ test/raw_solver.jl
@@ -1,11 +1,11 @@
 @testset "Raw solver: internals" begin
     models = [
-        sudoku(2; modeler = :raw),
+        sudoku(2; modeler=:raw),
     ]
 
     for m in models
         @info describe(m)
-        s = solver(m; options = Options(print_level =:verbose, iteration = Inf))
+        s = solver(m; options=Options(print_level=:verbose, time_limit = Inf, iteration=Inf, info_path="info.json"))
         for x in keys(get_variables(s))
             @test get_name(s, x) == "x$x"
             for c in get_cons_from_var(s, x)
@@ -40,66 +40,68 @@
             LocalSearchSolvers.tabu_value(s, x)
         end
         LocalSearchSolvers._values!(s, Dictionary{Int,Int}())
+        
+        # display(solution(s))
+        @info time_info(s)
+        # rm("info.json")
     end
 end
 
-@testset "Raw solver: sudoku" begin
-    sudoku_instance = collect(Iterators.flatten([
-        9  3  0  0  0  0  0  4  0
-        0  0  0  0  4  2  0  9  0
-        8  0  0  1  9  6  7  0  0
-        0  0  0  4  7  0  0  0  0
-        0  2  0  0  0  0  0  6  0
-        0  0  0  0  2  3  0  0  0
-        0  0  8  5  3  1  0  0  2
-        0  9  0  2  8  0  0  0  0
-        0  7  0  0  0  0  0  5  3
-    ]))
+# @testset "Raw solver: sudoku" begin
+#     sudoku_instance = collect(Iterators.flatten([
+#         9  3  0  0  0  0  0  4  0
+#         0  0  0  0  4  2  0  9  0
+#         8  0  0  1  9  6  7  0  0
+#         0  0  0  4  7  0  0  0  0
+#         0  2  0  0  0  0  0  6  0
+#         0  0  0  0  2  3  0  0  0
+#         0  0  8  5  3  1  0  0  2
+#         0  9  0  2  8  0  0  0  0
+#         0  7  0  0  0  0  0  5  3
+#     ]))
 
-    s = solver(sudoku(3; start = sudoku_instance, modeler = :raw); options = Options(print_level = :minimal, iteration = 100000, info_path = "info.json"))
-    display(Dictionary(1:length(sudoku_instance), sudoku_instance))
-    solve!(s)
-    display(solution(s))
-    @info time_info(s)
-    rm("info.json")
-end
+#     s = solver(sudoku(3; start = sudoku_instance, modeler = :raw); options = Options(print_level = :minimal, iteration = 100000))
+#     display(Dictionary(1:length(sudoku_instance), sudoku_instance))
+#     solve!(s)
+#     display(solution(s))
+# end
 
-@testset "Raw solver: golomb" begin
-    s = solver(golomb(5, modeler = :raw); options = Options(print_level = :minimal, iteration = 1000))
-    solve!(s)
+# @testset "Raw solver: golomb" begin
+#     s = solver(golomb(5, modeler = :raw); options = Options(print_level = :minimal, iteration = 1000))
+#     solve!(s)
 
-    @info "Results golomb!"
-    @info "Values: $(get_values(s))"
-    @info "Sol (val): $(best_value(s))"
-    @info "Sol (vals): $(!isnothing(best_value(s)) ? best_values(s) : nothing)"
-end
+#     @info "Results golomb!"
+#     @info "Values: $(get_values(s))"
+#     @info "Sol (val): $(best_value(s))"
+#     @info "Sol (vals): $(!isnothing(best_value(s)) ? best_values(s) : nothing)"
+# end
 
-@testset "Raw solver: mincut" begin
-    graph = zeros(5, 5)
-    graph[1,2] = 1.0
-    graph[1,3] = 2.0
-    graph[1,4] = 3.0
-    graph[2,5] = 1.0
-    graph[3,5] = 2.0
-    graph[4,5] = 3.0
-    s = solver(mincut(graph, source=1, sink=5), options =  Options(print_level = :minimal))
-    solve!(s)
-    @info "Results mincut!"
-    @info "Values: $(get_values(s))"
-    @info "Sol (val): $(best_value(s))"
-    @info "Sol (vals): $(!isnothing(best_value(s)) ? best_values(s) : nothing)"
+# @testset "Raw solver: mincut" begin
+#     graph = zeros(5, 5)
+#     graph[1,2] = 1.0
+#     graph[1,3] = 2.0
+#     graph[1,4] = 3.0
+#     graph[2,5] = 1.0
+#     graph[3,5] = 2.0
+#     graph[4,5] = 3.0
+#     s = solver(mincut(graph, source=1, sink=5), options =  Options(print_level = :minimal))
+#     solve!(s)
+#     @info "Results mincut!"
+#     @info "Values: $(get_values(s))"
+#     @info "Sol (val): $(best_value(s))"
+#     @info "Sol (vals): $(!isnothing(best_value(s)) ? best_values(s) : nothing)"
 
-    s = solver(mincut(graph, source=1, sink=5, interdiction=1), options =  Options(print_level = :minimal))
-    solve!(s)
-    @info "Results 1-mincut!"
-    @info "Values: $(get_values(s))"
-    @info "Sol (val): $(best_value(s))"
-    @info "Sol (vals): $(!isnothing(best_value(s)) ? best_values(s) : nothing)"
+#     s = solver(mincut(graph, source=1, sink=5, interdiction=1), options =  Options(print_level = :minimal))
+#     solve!(s)
+#     @info "Results 1-mincut!"
+#     @info "Values: $(get_values(s))"
+#     @info "Sol (val): $(best_value(s))"
+#     @info "Sol (vals): $(!isnothing(best_value(s)) ? best_values(s) : nothing)"
 
-    s = solver(mincut(graph, source=1, sink=5, interdiction=2), options =  Options(print_level = :minimal))
-    solve!(s)
-    @info "Results 2-mincut!"
-    @info "Values: $(get_values(s))"
-    @info "Sol (val): $(best_value(s))"
-    @info "Sol (vals): $(!isnothing(best_value(s)) ? best_values(s) : nothing)"
-end
+#     s = solver(mincut(graph, source=1, sink=5, interdiction=2), options =  Options(print_level = :minimal))
+#     solve!(s)
+#     @info "Results 2-mincut!"
+#     @info "Values: $(get_values(s))"
+#     @info "Sol (val): $(best_value(s))"
+#     @info "Sol (vals): $(!isnothing(best_value(s)) ? best_values(s) : nothing)"
+# end
