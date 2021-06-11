e3b7406cf6ddaef3007c3eb0a347d4fdf13c661f
Azzaare <jf@baffier.fr>
2021-06-11 17:46:24 +0900

    Fix arg order in Fluct

--- src/fluct.jl
+++ src/fluct.jl
@@ -1,9 +1,10 @@
 struct Fluct
     cons::Dictionary{Int, Float64}
     vars::Dictionary{Int, Float64}
+    Fluct(cons, vars) = new(zeros(cons), zeros(vars))
 end
 
-Fluct(cons, vars) = Fluct(zeros(cons), zeros(vars))
+
 
 function reset!(fluct)
     zeros! = d -> foreach(k -> set!(d, k, 0.0), keys(d))
@@ -12,11 +13,11 @@ function reset!(fluct)
 end
 
 function copy_to!(fluct, cons, vars)
-    foreach(k -> set!(fluct.cons, k, cons[k]), cons)
-    foreach(k -> set!(fluct.vars, k, vars[k]), vars)
+    foreach(k -> set!(fluct.cons, k, cons[k]), keys(cons))
+    foreach(k -> set!(fluct.vars, k, vars[k]), keys(vars))
 end
 
 function copy_from!(fluct, cons, vars)
-    foreach(k -> set!(cons, k, fluct.cons[k]), cons)
-    foreach(k -> set!(vars, k, fluct.vars[k]), vars)
+    foreach(k -> set!(cons, k, fluct.cons[k]), keys(cons))
+    foreach(k -> set!(vars, k, fluct.vars[k]), keys(vars))
 end
